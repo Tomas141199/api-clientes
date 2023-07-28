f3ac7703c52a37efcadfa5e2f65521315b7020ba
Tomas141199 <tomashernandez141199@gmail.com>
2023-07-27 20:29:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+const cors = require("cors");
+require("dotenv").config();
 const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+const openApiConfiguration = require("./docs/swagger");
+const dbConnectNoSql = require("./config/mongoDb");
 
 const app = express();
-const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT}`);
-});
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running");
-});
+/**
+ * ENV vars
+ */
+const PORT = process.env.PORT || 4000;
+
+/**
+ * API Documentation
+ */
+app.use(
+  "/documentation",
+  swaggerUI.serve,
+  swaggerUI.setup(openApiConfiguration)
+);
+
+/**
+ * Invoke routes
+ */
+app.use("/api", require("./routes"));
+
+app.listen(PORT);
+
+dbConnectNoSql();
 
 module.exports = app;
